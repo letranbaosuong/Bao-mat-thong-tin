1ddde11d5ca273d2d50faed827bdb3993e8a2874
letranbaosuong <letranbaosuong@gmail.com>
2020-04-27 16:48:46 +0700

    done Hamming

--- De_Tai_Mon_Hoc_Java/Hamming/app/src/main/java/com/gdu/letranbaosuong/dhpm11/hamming/MainActivity.java
+++ De_Tai_Mon_Hoc_Java/Hamming/app/src/main/java/com/gdu/letranbaosuong/dhpm11/hamming/MainActivity.java
@@ -8,11 +8,14 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Entity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.graphics.pdf.PdfDocument;
 import android.os.Build;
 import android.os.Bundle;
@@ -27,13 +30,15 @@ import android.widget.EditText;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Type;
 
 @RequiresApi(api = Build.VERSION_CODES.KITKAT)
 public class MainActivity extends AppCompatActivity {
     private Button btnTinhToan;
     private EditText txtAHexStr, txtBHexStr;
     private String txtABinStr, txtATrongSo, txtBBinStr, txtBTrongSo, txtAxorBHexStr, txtAxorBBinStr, txtKhoangCach;
-    private Bitmap bmp;
+    private Bitmap bmp, scaleBitmap;
+    private int pageWidth = 1200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void TinhToan() {
         btnTinhToan.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
                 XuLyTinhToan();
@@ -65,28 +69,38 @@ public class MainActivity extends AppCompatActivity {
         int TrongSoAxorB = BinStrToTrongSoHamming(txtAxorBBinStr);
         int KhoangCachHammingAvaB = BinStrKhoangCachHamming(txtABinStr, txtBBinStr);
         txtKhoangCach = String.valueOf(KhoangCachHammingAvaB);
-        if (TrongSoAxorB == KhoangCachHammingAvaB) {
-            CreatePDF();
-            OpenDialog("Chương trình Hamming tính đúng trọng số và khoảng cách");
-        } else {
-            OpenDialog("Chương trình bị lỗi sai");
-        }
+        OpenDialog(TrongSoAxorB == KhoangCachHammingAvaB);
     }
 
-    private void OpenDialog(String s) {
+    private void OpenDialog(boolean kiemTra) {
 //        MyDialog myDialog = new MyDialog();
 //        myDialog.show(getSupportFragmentManager(), "example dialog");
 
-        new AlertDialog.Builder(MainActivity.this)
-                .setTitle("Bài toán Hamming")
-                .setMessage(s)
-                .setCancelable(false)
-                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+        if (kiemTra) {
+            CreatePDF();
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Bài toán Hamming")
+                    .setMessage("Chương trình Hamming tính đúng trọng số và khoảng cách")
+                    .setCancelable(false)
+                    .setPositiveButton("Xem", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(MainActivity.this, ViewPDFActivity.class);
+                            startActivity(intent);
+                        }
+                    }).show();
+        } else {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Bài toán Hamming")
+                    .setMessage("Chương trình bị lỗi sai")
+                    .setCancelable(false)
+                    .setPositiveButton("Đóng", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
 
-                    }
-                }).show();
+                        }
+                    }).show();
+        }
     }
 
     private void XuLy() {
@@ -129,7 +143,8 @@ public class MainActivity extends AppCompatActivity {
         btnTinhToan = findViewById(R.id.btnTinhToan);
         txtAHexStr = findViewById(R.id.txtAHexStr);
         txtBHexStr = findViewById(R.id.txtBHexStr);
-        bmp= BitmapFactory.decodeResource(getResources(),R.drawable.axorb);
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.axorb);
+        scaleBitmap = Bitmap.createScaledBitmap(bmp, 50, 30, false);
     }
 
     private void CapQuyen() {
@@ -142,20 +157,117 @@ public class MainActivity extends AppCompatActivity {
 
         PdfDocument myPdfDocument = new PdfDocument();
         Paint myPaint = new Paint();
+        Paint titlePaint = new Paint();
 
-        PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(250, 400, 1).create();
+        PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(pageWidth, 2010, 1).create();
         PdfDocument.Page myPage = myPdfDocument.startPage(myPageInfo);
         Canvas canvas = myPage.getCanvas();
-        canvas.drawText("Lê Trần Bảo Sương - Trang 1", 40, 50, myPaint);
+
+        // Title
+        titlePaint.setTextAlign(Paint.Align.CENTER);
+        titlePaint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD_ITALIC));
+        titlePaint.setTextSize(70);
+        canvas.drawText("Hamming", pageWidth / 2, 270, titlePaint);
+
+        // Row - khung - title
+//        myPaint.setStyle(Paint.Style.STROKE);
+//        myPaint.setStrokeWidth(2);
+        myPaint.setColor(Color.RED);
+        canvas.drawRect(20, 300, pageWidth - 20, 340, myPaint);
+
+        // Row - title
+        myPaint.setTextAlign(Paint.Align.LEFT);
+        myPaint.setStyle(Paint.Style.FILL);
+        myPaint.setTextSize(20);
+        myPaint.setColor(Color.WHITE);
+        canvas.drawText("16 ký số Hex", 40, 330, myPaint);
+        canvas.drawText("Số Hexadecimal", 200, 330, myPaint);
+        canvas.drawText("Số nhị phân", 400, 330, myPaint);
+        canvas.drawText("Trọng số Hamming", 1000, 330, myPaint);
+
+        myPaint.setStyle(Paint.Style.STROKE);
+        myPaint.setStrokeWidth(2);
+        myPaint.setColor(Color.BLACK);
+        canvas.drawLine(20, 300, pageWidth - 20, 300, myPaint);
+        canvas.drawLine(20, 300, 20, 340, myPaint);
+        canvas.drawLine(180, 300, 180, 340, myPaint);
+        canvas.drawLine(380, 300, 380, 340, myPaint);
+        canvas.drawLine(980, 300, 980, 340, myPaint);
+        canvas.drawLine(pageWidth - 20, 300, pageWidth - 20, 340, myPaint);
+
+        // Row - khung - A
+        myPaint.setStyle(Paint.Style.STROKE);
+        myPaint.setStrokeWidth(2);
+        canvas.drawRect(20, 340, pageWidth - 20, 380, myPaint);
+
+        // Row - A
+        myPaint.setTextAlign(Paint.Align.LEFT);
+        myPaint.setStyle(Paint.Style.FILL);
+        myPaint.setTextSize(16);
+        canvas.drawText("A", 40, 370, myPaint);
+        canvas.drawText(txtAHexStr.getText().toString().trim(), 200, 370, myPaint);
+        canvas.drawText(txtABinStr, 400, 370, myPaint);
+        canvas.drawText(txtATrongSo, 1000, 370, myPaint);
+
+        canvas.drawLine(180, 340, 180, 380, myPaint);
+        canvas.drawLine(380, 340, 380, 380, myPaint);
+        canvas.drawLine(980, 340, 980, 380, myPaint);
+
+        // Row - khung - B
+        myPaint.setStyle(Paint.Style.STROKE);
+        myPaint.setStrokeWidth(2);
+        canvas.drawRect(20, 380, pageWidth - 20, 420, myPaint);
+
+        // Row - B
+        myPaint.setTextAlign(Paint.Align.LEFT);
+        myPaint.setStyle(Paint.Style.FILL);
+        myPaint.setTextSize(16);
+        canvas.drawText("B", 40, 410, myPaint);
+        canvas.drawText(txtBHexStr.getText().toString().trim(), 200, 410, myPaint);
+        canvas.drawText(txtBBinStr, 400, 410, myPaint);
+        canvas.drawText(txtBTrongSo, 1000, 410, myPaint);
+
+        canvas.drawLine(180, 380, 180, 420, myPaint);
+        canvas.drawLine(380, 380, 380, 420, myPaint);
+        canvas.drawLine(980, 380, 980, 420, myPaint);
+
+        // Row - khung - khoang cach hamming
+//        myPaint.setStyle(Paint.Style.STROKE);
+//        myPaint.setStrokeWidth(2);
+        myPaint.setColor(Color.GREEN);
+        canvas.drawRect(20, 420, pageWidth - 20, 460, myPaint);
+
+        // Row - khoang cach hamming
+        myPaint.setTextAlign(Paint.Align.LEFT);
+        myPaint.setStyle(Paint.Style.FILL);
+        myPaint.setTextSize(16);
+        myPaint.setColor(Color.BLUE);
+        canvas.drawText("Khoảng cách Hamming", 1000, 450, myPaint);
+
+        myPaint.setColor(Color.GREEN);
+        canvas.drawLine(980, 420, 980, 460, myPaint);
+
+        // Row - khung - AxorB
+        myPaint.setStyle(Paint.Style.STROKE);
+        myPaint.setStrokeWidth(2);
+        myPaint.setColor(Color.BLACK);
+        canvas.drawRect(20, 460, pageWidth - 20, 500, myPaint);
+
+        // Row - AxorB
+        myPaint.setTextAlign(Paint.Align.LEFT);
+        myPaint.setStyle(Paint.Style.FILL);
+        myPaint.setTextSize(16);
+        canvas.drawBitmap(scaleBitmap, 40, 465, myPaint);
+        canvas.drawText(txtAxorBHexStr, 200, 490, myPaint);
+        canvas.drawText(txtAxorBBinStr, 400, 490, myPaint);
+        canvas.drawText(txtKhoangCach, 1000, 490, myPaint);
+
+        canvas.drawLine(180, 460, 180, 500, myPaint);
+        canvas.drawLine(380, 460, 380, 500, myPaint);
+        canvas.drawLine(980, 460, 980, 500, myPaint);
+
         myPdfDocument.finishPage(myPage);
-
-        PdfDocument.PageInfo myPageInfo1 = new PdfDocument.PageInfo.Builder(250, 400, 1).create();
-        PdfDocument.Page myPage1 = myPdfDocument.startPage(myPageInfo1);
-        Canvas canvas1 = myPage1.getCanvas();
-        canvas1.drawText("Lê Trần Bảo Sương - Trần Nguyên Vẹn 2", 40, 50, myPaint);
-        myPdfDocument.finishPage(myPage1);
-
-        File file = new File(Environment.getExternalStorageDirectory(), "/TenTuiPDF.pdf");
+        File file = new File(Environment.getExternalStorageDirectory(), "/Hamming.pdf");
 
         try {
             myPdfDocument.writeTo(new FileOutputStream(file));
---
+++ De_Tai_Mon_Hoc_Java/Hamming/app/src/main/java/com/gdu/letranbaosuong/dhpm11/hamming/ViewPDFActivity.java
@@ -0,0 +1,28 @@
+package com.gdu.letranbaosuong.dhpm11.hamming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.File;
+
+public class ViewPDFActivity extends AppCompatActivity {
+    private PDFView pdfView;
+    private File file;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pdf);
+        pdfView = findViewById(R.id.pdfView);
+        file = new File("/sdcard/Hamming.pdf");
+        pdfView.fromFile(file)
+                .enableSwipe(true)
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .enableAntialiasing(true)
+                .load();
+    }
+}
